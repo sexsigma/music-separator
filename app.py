4e845e8c5dc4bb5927faa5ccfe494b083af84b30
Sigmame <i@sigma.me>
2024-12-24 17:28:18 +0800

    Add render.yaml

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def download_file(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
